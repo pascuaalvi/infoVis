df46dd1dfd994ecdddf609e560b5e987f39e5b53
samuraiRAZR <alvinpascual21@ymail.com>
2015-11-09 15:11:18 +1300

    Added pie charts for most questions

--- public/scripts/details.js
+++ public/scripts/details.js
@@ -37,77 +37,200 @@ $( document ).ready(function(){
     }
   }
 
-function gender(){  
+function pie(context){  
   var country = $("#countryName")[0].innerText;
   var vis = d3.select("#vizArea").selectAll("*").remove(); 
   var aClient = new HttpClient();
 
-  var width = 800,
-    height = 250,
-    radius = Math.min(width, height) / 2;
-
-  aClient.get("/data?country='"+country+"'&context=gender", function (response) {
+  aClient.get("/data?country='"+country+"'&context="+context, function (response) {
     
     var array = JSON.parse(response);
-
-    var total = response.cp + response.cf + response.ncp + response.ncf;
     
-    var cp = array["cp"];
-    var cf = array["cf"];
-    var ncp = array["ncp"];
-    var ncf = array["ncf"];
+    var data1 = [];
+    var data2 = [];
 
-    var data = [
-    {"context":"Coed School - Passed","tally":  cp},
-    {"context":"Coed School - Failed","tally": cf},
-    {"context":"Segregated School - Passed","tally": ncp},
-    {"context":"Segregated School - Failed","tally": ncf}
-    ];
+    if(context === "gender"){
+      var data1 = [
+        {"context":"Coed School - Passed","tally": array["cp"]},
+        {"context":"Coed School - Failed","tally": array["cf"]}
+        ];
+
+      var data2 = [
+        {"context":"Segregated School - Passed","tally": array["ncp"]},
+        {"context":"Segregated School - Failed","tally": array["ncf"]}
+        ];
+    }
+    else if(context === "type"){
+      var data1 = [
+        {"context":"Private School - Passed","tally": array["pPrivate"]},
+        {"context":"Private School - Failed","tally": array["fPrivate"]}
+        ];
+
+      var data2 = [
+        {"context":"Public School - Passed","tally": array["pPublic"]},
+        {"context":"Public School - Failed","tally": array["fPublic"]}
+        ];
+    }
+    else if(context === "sports"){
+      var data1 = [
+        {"context":"School has Sport Clubs - Passed","tally": array["sp"]},
+        {"context":"School has Sport Clubs - Failed","tally": array["sf"]}
+        ];
+
+      var data2 = [
+        {"context":"School doesn't have Sport Clubs - Passed","tally": array["nsp"]},
+        {"context":"School doesn't have Sport Clubs - Failed","tally": array["nsf"]}
+        ];
+    }
+    else if(context === "arts"){
+      var data1 = [
+        {"context":"School has Art Clubs - Passed","tally": array["ap"]},
+        {"context":"School has Art Clubs - Failed","tally": array["af"]}
+        ];
+
+      var data2 = [
+        {"context":"School doesn't have Art Clubs - Passed","tally": array["nap"]},
+        {"context":"School doesn't have Art Clubs - Failed","tally": array["naf"]}
+        ];
+    }
+    
 
     console.log(response);
 
-    var width = 800,
-    height = 250,
+    var width = 1200,
+    height = 500,
     radius = Math.min(width, height) / 2;
 
     var color = d3.scale.ordinal()
-        .range(["#00aa00", "#55aa00", "#aa5500", "#aa0000"]);
+        .range(["#00aa00", "#ff0000"]);
+
+    var svg1 = d3.select("#vizArea").append("svg")
+        .attr("width", width/2)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 4 + "," + height / 2 + ")");
+
+    var svg2 = d3.select("#vizArea").append("svg")
+      .attr("width", width/2)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 4 + "," + height / 2 + ")");
+
+    var pie = d3.layout.pie()
+      .sort(null)
+      .startAngle(0*Math.PI)
+      .endAngle(2*Math.PI)
+      .value(function (d) {
+        return d.tally;
+      });
+
+    // var svg = d3.select("#vizArea").append("div")
+    //   .attr("width", width)
+    //   .attr("height", height)
+    //   .append("g")
+    //   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // FIRST GRAPH
 
     var arc = d3.svg.arc()
         .outerRadius(radius - 50)
-        .innerRadius(radius - 70);
+        .innerRadius(radius - 100);
 
-    var pie = d3.layout.pie()
-        .sort(null)
-        .value(function (d) {
-        return d.tally;
-    });
-
-    var svg = d3.select("#vizArea").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-        var g = svg.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
+    var g = svg1.selectAll(".arc")
+      .data(pie(data1))
+      .enter().append("g")
+      .attr("class", "arc");
+      // .on("mouseover", function (d) {
+      //   d3.select("#tooltip")
+      //       .style("left", d3.event.pageX + "px")
+      //       .style("top", d3.event.pageY + "px")
+      //       .style("opacity", 1)
+      //       .select("#value")
+      //       .text(d.value);
 
         g.append("path")
-            .attr("d", arc)
             .style("fill", function (d) {
-            return color(d.data.context);
+              return color(d.data.context);
+            })
+            .style('stroke', 'white')
+            .style('stroke-width', 10)
+            .transition().delay(function(d, i) { return i * 1000; }).duration(1000)
+            .attrTween('d', function(d) {
+                var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
+                return function(t) {
+                    d.endAngle = i(t);
+                  return arc(d);
+                }
+              });
+
+        // Labels
+        g.append("text")
+          .attr("transform", function (d) {
+          return "translate(" + arc.centroid(d) + ")";
+          })
+          .attr("text-anchor", "middle")
+          .attr("fill", "white")
+          .text(function (d) {
+          return d.data.tally;
         });
 
-        g.append("text")
-            .attr("transform", function (d) {
-                return "translate(" + arc.centroid(d) + ")";
+    // SECOND GRAPH
+
+    var arc2 = d3.svg.arc()
+      .outerRadius(radius - 50)
+      .innerRadius(radius - 100);
+
+    var g2 = svg2.selectAll(".arc")
+      .data(pie(data2))
+      .enter().append("g")
+      .attr("class", "arc");
+      // .on("mouseover", function (d) {
+      //   d3.select("#tooltip")
+      //       .style("left", d3.event.pageX + "px")
+      //       .style("top", d3.event.pageY + "px")
+      //       .style("opacity", 1)
+      //       .select("#value")
+      //       .text(d.value);
+
+        g2.append("path")
+            .style("fill", function (d) {
+              return color(d.data.context);
             })
-            .attr("dy", ".35em")
-            .style("text-anchor", "middle")
-            .text(function (d) {
-                return d.data.context;
-            });
-    });  
+            .style('stroke', 'white')
+            .style('stroke-width', 10)
+            .transition().delay(function(d, i) { return i * 1000; }).duration(1000)
+            .attrTween('d', function(d) {
+                var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
+                return function(t) {
+                    d.endAngle = i(t);
+                  return arc(d);
+                }
+              });
+
+        // Labels
+        g2.append("text")
+          .attr("transform", function (d) {
+          return "translate(" + arc.centroid(d) + ")";
+          })
+          .attr("text-anchor", "middle")
+          .attr("fill", "white")
+          .text(function (d) {
+          return d.data.tally;
+        });
+
+        // END
+  });  
+}
+
+function bar(){
+  var country = $("#countryName")[0].innerText;
+  var vis = d3.select("#vizArea").selectAll("*").remove(); 
+  var aClient = new HttpClient();
+
+  aClient.get("/data?country='"+country+"'&context=location", function (response) {
+    
+    var array = JSON.parse(response);
+
+
+  });
 }
